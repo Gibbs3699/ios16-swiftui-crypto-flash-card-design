23af743c96254819946fe46e698df7a0ac878c61
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-18 21:52:59 +0700

    init project

---
+++ Crypto/Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by TheGIZzz on 18/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
