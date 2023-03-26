2f5a1a3755288b4a89603b768f73c4aff8a9dad2
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-26 23:13:11 +0700

    organize folders for core and component in views

--- Crypto/Crypto/Views/Core/View/HomeView.swift
+++ Crypto/Crypto/Views/Core/View/HomeView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Header")
+                Spacer(minLength: 0)
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .navigationBarHidden(true)
     }
 }
