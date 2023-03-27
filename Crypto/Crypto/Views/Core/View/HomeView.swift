eb08a8a170b7c96cb9394906dda46772b96fe3ad
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-27 15:32:07 +0700

    clean up homeView

--- Crypto/Crypto/Views/Core/View/HomeView.swift
+++ Crypto/Crypto/Views/Core/View/HomeView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
             
             VStack {
-                Text("Header")
+                homeView
+                
                 Spacer(minLength: 0)
             }
         }
@@ -25,5 +29,32 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
             .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    
+    var homeView: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: 0)
+                .background(
+                    CircleButtonAnimation(animate: $showPortfolio)
+                )
+            Spacer()
+            
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+            
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding()
     }
 }
