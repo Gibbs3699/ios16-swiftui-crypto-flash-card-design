b4baf92e40232ccd890afa391de9bd10b4347018
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-30 18:48:29 +0700

    add CoinRowView

---
+++ Crypto/Crypto/Views/Core/View/CoinRowView.swift
@@ -0,0 +1,29 @@
+//
+//  CoinRowView.swift
+//  Crypto
+//
+//  Created by TheGIZzz on 30/3/2566 BE.
+//
+
+import Foundation
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: CoinModel
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+        }
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin)
+    }
+}
