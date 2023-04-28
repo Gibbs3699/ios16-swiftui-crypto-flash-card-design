43951915f516cebcf0ad99c58a877a8f17a833c6
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-04-28 15:22:51 +0700

    add WalletCardView and card assets

---
+++ Crypto/Crypto/Views/Core/View/WalletCardView.swift
@@ -0,0 +1,20 @@
+//
+//  WalletCardView.swift
+//  Crypto
+//
+//  Created by TheGIZzz on 28/4/2566 BE.
+//
+
+import SwiftUI
+
+struct WalletCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WalletCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        WalletCardView()
+    }
+}
