4120acc70d31f446d3e3bf4e04f016ce931bf7cd
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-05-24 22:36:07 +0700

    add disable autocorrect

--- Crypto/Crypto/Views/Components/SearchBarView.swift
+++ Crypto/Crypto/Views/Components/SearchBarView.swift
@@ -20,6 +20,7 @@ struct SearchBarView: View {
             
             TextField("Search by name or symbol...", text: $searchText)
                 .foregroundColor(Color.theme.accent)
+                .disableAutocorrection(true)
                 .overlay(
                     Image(systemName: "xmark.circle.fill")
                         .padding()
